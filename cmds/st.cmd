f1f04c2ab9ec0166db3ae5d6729e631be100d86c
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2023-09-01 13:33:46 +0200

    The first version having connection to the instrument.

--- cmds/st.cmd
+++ cmds/st.cmd
@@ -1,3 +1,5 @@
 # This should be a test or example startup script
 
 require n1912a
+require afterinit
+iocshLoad("$(n1912a_DIR)n1912a.iocsh", "IP=192.168.10.35,P=HPRF-PwrMtr:,IOCNAME=ioc30-n1912,TOP=/tmp/autosave")
