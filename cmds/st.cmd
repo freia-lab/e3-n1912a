a6d3274ef85378a81344463f79bd9be350f58043
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2025-02-03 09:24:07 +0100

    Update required module version. Changed IP address.

--- cmds/st.cmd
+++ cmds/st.cmd
@@ -2,4 +2,4 @@
 
 require n1912a
 require afterinit
-iocshLoad("$(n1912a_DIR)n1912a.iocsh", "IP=192.168.10.35,P=HPRF-PwrMtr:,IOCNAME=ioc30-n1912,TOP=/tmp/autosave")
+iocshLoad("$(n1912a_DIR)n1912a.iocsh", "IP=192.168.10.36,P=HPRF-PwrMtr:,IOCNAME=ioc30-n1912,TOP=/tmp/autosave")
