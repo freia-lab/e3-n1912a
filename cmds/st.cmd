cd9e4789e4c659a8fe5062424f7dbf8e847d8bf5
Marek Jacewicz <marek.jacewicz@physics.uu.se>
2025-02-07 16:52:44 +0100

    Add new records and update protocol"

--- cmds/st.cmd
+++ cmds/st.cmd
@@ -1,5 +1,7 @@
 # This should be a test or example startup script
 
 require n1912a
-require afterinit
-iocshLoad("$(n1912a_DIR)n1912a.iocsh", "IP=192.168.10.36,P=HPRF-PwrMtr:,IOCNAME=ioc30-n1912,TOP=/tmp/autosave")
+# does not seem to be needed, it will be loaded automatically, 
+# since module dependency is stated in CONFIG_MODULE
+#require afterinit
+iocshLoad("$(n1912a_DIR)n1912a.iocsh", "IP=192.168.10.36,IP_PORT=5025,P=HPRF-PwrMtr:,IOCNAME=ioc30-n1912,TOP=/tmp/autosave")
