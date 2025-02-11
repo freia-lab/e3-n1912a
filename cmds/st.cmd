9878cdecea902628ce54e9a189b1063f324239bd
Marek Jacewicz <marek.jacewicz@physics.uu.se>
2025-02-11 17:21:26 +0100

    Working version with proper names

--- cmds/st.cmd
+++ cmds/st.cmd
@@ -4,4 +4,4 @@ require n1912a
 # does not seem to be needed, it will be loaded automatically, 
 # since module dependency is stated in CONFIG_MODULE
 #require afterinit
-iocshLoad("$(n1912a_DIR)n1912a.iocsh", "IP=192.168.10.36,IP_PORT=5025,P=HPRF-PwrMtr:,IOCNAME=ioc30-n1912,TOP=/tmp/autosave")
+iocshLoad("$(n1912a_DIR)n1912a.iocsh", "IP=192.168.10.36,IP_PORT=5025,P=LLRF-Ctrl:,DEV1=Channel1,DEV2=Channel2,IOCNAME=ioc30-n1912,TOP=/tmp/autosave")
