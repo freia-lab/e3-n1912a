d7839f13547e63bda7b4012b93cd4340213202b7
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2025-02-13 15:59:53 +0100

    The first installed version of the module.

--- cmds/st.cmd
+++ cmds/st.cmd
@@ -1,7 +1,11 @@
 # This should be a test or example startup script
 
 require n1912a
+epicsEnvSet("IOCNAME", "ioc-test")
+
 # does not seem to be needed, it will be loaded automatically, 
 # since module dependency is stated in CONFIG_MODULE
 #require afterinit
-iocshLoad("$(n1912a_DIR)n1912a.iocsh", "IP=192.168.10.36,IP_PORT=5025,P=LLRF-Ctrl:,DEV1=Channel1,DEV2=Channel2,IOCNAME=ioc30-n1912,TOP=/tmp/autosave")
+iocshLoad("$(n1912a_DIR)n1912a.iocsh", "IP=192.168.10.31,IP_PORT=5025,P=LLRF-Ctrl:,DEV1=FwdPwr,DEV2=ReflPwr,N=1,IOCNAME=ioc30-n1912,TOP=/tmp/autosave")
+
+dbLoadRecords("asynRecord.db","P=${IOCNAME},R=:asynRec,PORT=$(ASYN_PORT=PORT1),ADDR='0',IMAX='1024',OMAX='256'")
